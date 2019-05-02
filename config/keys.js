afe321096d17b5ca88282eda844831c76c2a532b
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-02 12:08:17 -0600

    Changed entrance URI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   // mongoURI : 'mongodb+srv://Tucker:Tucker@cluster0-tihhu.mongodb.net/ReactApp?retryWrites=true'
-  mongoURI : 'mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+  mongoURI : 'mongodb://Tucker:Tucker@cluster0-shard-00-00-tihhu.mongodb.net:27017,cluster0-shard-00-01-tihhu.mongodb.net:27017,cluster0-shard-00-02-tihhu.mongodb.net:27017/ReactApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 }
